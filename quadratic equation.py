27e749408c170b3668b6e983444255147f9e0978
pooji1475 <112894452+pooji1475@users.noreply.github.com>
2022-09-05 23:03:11 +0530

    Add files via upload

---
+++ quadratic equation.py
@@ -0,0 +1,11 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================================ RESTART: C:/Users/Poojitha/AppData/Local/Programs/Python/Python310/greatest of 3 nums.py ===============================
+30
+
+================================ RESTART: C:/Users/Poojitha/AppData/Local/Programs/Python/Python310/greatest of 3 nums.py ===============================
+enter value of a4
+enter value of b6
+enter value of c7
+The solution are (-0.75-1.0897247358851685j) and (-0.75+1.0897247358851685j)
