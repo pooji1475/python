17889484b5c1aa6b1f638f60f0ab03852c04d53c
pooji1475 <112894452+pooji1475@users.noreply.github.com>
2022-09-09 08:16:14 +0530

    Add files via upload

---
+++ rectangle star pattren.py
@@ -0,0 +1,8 @@
+rows=int(input("enter total number of rows:"))
+columns=int(input("enter total number of columns:"))
+print("rectangle star pattren")
+for i in range(rows):
+    for j in range(columns):
+        print('*',end=' ')
+    print()
+            
