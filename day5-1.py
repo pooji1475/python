67a6760043a566b024e185bafe95f365949d96b2
pooji1475 <112894452+pooji1475@users.noreply.github.com>
2022-10-10 11:19:12 +0530

    Add files via upload

--- day5-1.py
+++ day5-1.py
@@ -1,11 +1,6 @@
-def lengthoflastword(a):
-    l=0
-    x=a.strip()
-    for i in range(len(x)):
-        if(x[i]==" "):
-            l=0
-        else:
-            l+=1
-    return l
-string=input("Enter a string:")
-print(lengthoflastword(string))
+def length(str):
+    lis = list(str.split(" "))
+    return len(lis[-1])
+ 
+str = input("Enter the string: ")
+print("The length of the last word is",length(str))
